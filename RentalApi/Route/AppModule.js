a5581691b032bb4eaf5fcf8cc0f0248155a088bb
XuiKoh <silversui@hotmail.com>
2015-10-09 16:53:48 +1000

    Get Start here

--- RentalApi/Route/AppModule.js
+++ RentalApi/Route/AppModule.js
@@ -6,7 +6,7 @@ module.exports = {
 		flickrKeywordArray = flickrKeyword.split(',,');
 		tags = flickrKeywordArray[0];
 		page = flickrKeywordArray[2];
-		//JsonArray = [];
+		JsonArray = [];
 		if(flickrKeywordArray[1] == null)
 			keyword = "";
 		else
@@ -16,7 +16,7 @@ module.exports = {
 		+'&tag_mode=all&accuracy=1&extras=description&per_page=500&page='+ page +'&format=json&nojsoncallback=1';
 		request(url, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
-				var data = ({photos:{
+				data = ({photos:{
 									photo:[]
 									},
 							pages:0});
@@ -26,29 +26,36 @@ module.exports = {
 			//res.send(data);
 		});
 		function load2ndRequest(PagesResult){
-			for ( var spage =1 ; spage <= PagesResult ; spage ++ ){
+			for ( var spage =0 ; spage < PagesResult ; spage ++ ){
 				var surl = 'https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=5eded44188320998948b9966f4d49e50&text='+ keyword 
 				+'&tags='+ tags 
 				+'&tag_mode=all&accuracy=1&extras=description&per_page=500&page='+ spage +'&format=json&nojsoncallback=1';
 				request(surl, function(error, response, sbody) {
-					try{
-						var pinfo = JSON.parse(sbody);
-					}catch(e){
-						console.log("2nd RequestError Messages :"+e);
-					}
+					var info = JSON.parse(sbody);
 					
-					// console.log("Loading Page: " + spage +"/"+PagesResult);
-					//var addrssPat = /\d+\s+\w+\s+(?:st(?:\.|reet)?|ave(?:\.|nue)?|lane|dr(?:\.|ive)?)/i;
+					try{
+						console.log("LoadPage: "+info.photos.page + "/" +info.photos.pages);
+						var addrssPat = /\d+\s+\w+\s+(?:st(?:\.|reet)?|ave(?:\.|nue)?|lane|dr(?:\.|ive)?)/i;
 						// for ( var i = 0 ; i < info.photos.photo.length; i ++ ){
 						// 	if (addrssPat.test(info.photos.photo[i].title)){
 						// 		JsonArray.push(info.photos.photo[i]);
 						// 	}		
 						// }
-						//data.photos.photo = JsonArray;
-					//console.log("JsonArray Length " + JsonArray.length);
+						// data.photos.photo = JsonArray;
+						// console.log("1) JsonArray Length " + JsonArray.length);
+					}catch(e){
+						console.log("2nd RequestError Messages :"+e);
+						// console.log("Loading Page: " + pinfo.photos.page +"/"+pinfo.photos.pages);
+					}
+					console.log("2) JsonArray Length " + JsonArray.length);
+					console.log("Loading Page: " + spage +"/"+PagesResult);
+					
 				});
-				//data.photos.photo = JsonArray;
+				console.log("Forloop " + spage + "/"+ PagesResult);
+				
 			}
+			data.photos.photo = JsonArray;
+			res.send(data);
 		}
 	},
 
